0af02003184416b3e1d79a22285495659a94e6ad
김가영 <kkayoung0@gmail.com>
2024-01-29 22:58:14 +0900

    Refactor: 대본 관련 API에 로그인 유저 정보를 받지 않도록 수정 #291

--- BackEnd/src/main/java/com/ssafy/ploud/domain/script/controller/ScriptController.java
+++ BackEnd/src/main/java/com/ssafy/ploud/domain/script/controller/ScriptController.java
@@ -9,17 +9,17 @@ import com.ssafy.ploud.domain.script.dto.response.ScriptDetailResDto;
 import com.ssafy.ploud.domain.script.dto.response.ScriptListResDto;
 import com.ssafy.ploud.domain.script.service.ScriptService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "대본 관련 API")
+@SecurityRequirement(name = "Bearer Authentication")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/script/")
@@ -29,15 +29,14 @@ public class ScriptController {
 
   @Operation(summary = "모든 대본 카테고리 조회")
   @GetMapping("/list")
-  public ApiResponse<List<ScriptCategoriesResDto>> getAllScriptCategories(
-      @AuthenticationPrincipal UserDetails loginUser) {
+  public ApiResponse<List<ScriptCategoriesResDto>> getAllScriptCategories() {
     return ApiResponse.ok("모든 대본 카테고리 조회 성공", ScriptEntity.getAllScriptCategory());
   }
 
   @Operation(summary = "대본 목록 조회", description = "선택한 대본 카테고리에 해당하는 대본 조회")
   @GetMapping("/list/{categoryId}")
   public ApiResponse<ScriptListResDto> getAllScriptListByCategory(
-      @PathVariable("categoryId") int categoryId, @AuthenticationPrincipal UserDetails loginUser) {
+      @PathVariable("categoryId") int categoryId) {
 
     if (ScriptCategory.fromCategoryId(categoryId) == null) {
       return ApiResponse.failure("categoryId에 해당하는 카테고리가 존재하지 않습니다", ResponseStatus.NOT_FOUND);
