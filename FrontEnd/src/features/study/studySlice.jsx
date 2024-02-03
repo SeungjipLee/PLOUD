4839f7b39558e3b930f5fa0bbd4ee3257ccce8ae
junhyung <wnsgud0895@gmail.com>
2024-02-03 23:52:05 +0900

    Merge: conflict 수정

--- FrontEnd/src/features/study/studySlice.jsx
+++ FrontEnd/src/features/study/studySlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   studyList: [],
+  studyInfo: undefined,
 };
 
 export const studySlice = createSlice({
--- FrontEnd/src/pages/MainPage/index.jsx
+++ FrontEnd/src/pages/MainPage/index.jsx
@@ -23,51 +23,36 @@ const MainPage = () => {
     <div className="Main">
       <Page header={<Navbar />} footer={<Footer />}>
         <div className="Main1 mb-24">
-          <img
-            src="images/Main1.png"
-            alt="Main1.png"
-            className="w-full h-full object-cover"
-          />
+          <img src="images/Main1.png" alt="Main1.png" className="w-full h-full object-cover"/>
         </div>
-        <div className="Main2Container">
-          <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5">
-            Carousel
+        <div className="Main2 mb-24">
+          <div className="video mx-16 ms-36 me-12">
+            <video src="videos/cat.mp4" autoPlay loop />
           </div>
-          <div className="Main2Container">
-            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">
-              Carousel
-            </div>
-            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">
-              오늘의 발표 수
-            </div>
+          <div className='Main2Container'>
+            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">Carousel</div>
+            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">오늘의 발표 수</div>
           </div>
         </div>
         <div className="Main3">
-          <h2 className="text-5xl text-center pt-24 font-bold">
-            스피치 실력을 키워볼까요?
-          </h2>
-          <div className="Main3_1 mt-32">
+          <h2 className='text-5xl text-center pt-24 font-bold'>스피치 실력을 키워볼까요?</h2>
+          <div className='Main3_1 mt-32'>
             <div class="subtitleImg me-12 mb-24">
               <Link to="/practice">
-                <img src="images/solo.png" />
-                <div class="text">연습모드 바로가기</div>
+              <img src="images/solo.png"/>
+              <div class="text">연습모드 바로가기</div>
               </Link>
             </div>
             <div class="subtitleImg mb-24">
               <Link to="/study">
-                <img src="images/study.png" />
-                <div class="text">스터디룸 바로가기</div>
+              <img src="images/study.png"/>
+              <div class="text">스터디룸 바로가기</div>
               </Link>
             </div>
-            스터디룸 바로가기
           </div>
         </div>
-
         <div className="Main1 mb-24">
-          <img
-            src="images/evaluation2.PNG"
-            className="w-full h-full object-cover"
-          />
+          <img src="images/evaluation2.PNG" className="w-full h-full object-cover"/>
         </div>
       </Page>
     </div>
