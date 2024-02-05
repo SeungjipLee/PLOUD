dad1c33999178768553377da68950af57429fea1
junhyung <wnsgud0895@gmail.com>
2024-02-05 12:17:40 +0900

    Feat: 스터디비공개방 잠금이미지 추가

--- FrontEnd/src/pages/StudyPage/roomCard.jsx
+++ FrontEnd/src/pages/StudyPage/roomCard.jsx
@@ -12,20 +12,23 @@ const RoomCard = ({ data, children }) => {
     speechId,
     title,
   } = data;
-  
+
   return (
     <>
       <Card>
         <div className="card-study-room-info">
-          <h1>{title}</h1>
-          <div>
-          <p>{managerId}</p>
-          <p>{currentPeople} / {maxPeople}</p>
+          <div className="card-study-title">
+            <h1>{title}</h1>
+            {isPrivate && <img src="./images/private_icon.PNG" alt="" />}
           </div>
           <div>
-            {categoryId === 1 ? "발표" : "면접"}
+            <p>{managerId}</p>
+            <p>
+              {currentPeople} / {maxPeople}
+            </p>
           </div>
-          {children}  
+          <div>{categoryId === 1 ? "발표" : "면접"}</div>
+          {children}
         </div>
       </Card>
     </>
