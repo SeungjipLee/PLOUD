dcc43a96b087988ddd8e66f176bf1be9380f5e44
junhyung <wnsgud0895@gmail.com>
2024-02-05 23:26:55 +0900

    FIX: 화상회의 페이지 버튼 크기 축소

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
-import { StrictMode } from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./app/persistor";
-import "./index.css"
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 persistor;
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
