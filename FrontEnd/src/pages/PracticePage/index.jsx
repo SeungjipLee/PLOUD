1064966d5c05dff309567f262ed4c53eb3022153
이승집 <edward626kr@gmail.com>
2024-02-06 15:55:48 +0900

    Init: 결과화면 디자인 #422

--- FrontEnd/src/pages/PracticePage/index.jsx
+++ FrontEnd/src/pages/PracticePage/index.jsx
@@ -11,8 +11,6 @@ import {
   getScriptList,
   getScriptDetail,
 } from "../../services/script";
-import Page from "../../components/Page";
-import Footer from "../../components/Footer";
 
 const PracticePage = () => {
   const [currentPage, setCurrentPage] = useState("write");
@@ -23,61 +21,6 @@ const PracticePage = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <Page header={<Navbar />} footer={<Footer />}>
-      <div style={{ paddingTop: "130px" }}></div>
-      <div
-        style={{ paddingLeft: "120px" }}
-        className="text-3xl text-sky-950 font-bold "
-      >
-        혼자 연습
-      </div>
-      <div style={{ paddingLeft: "120px", paddingRight: "120px" }}>
-        <div style={{ paddingTop: "30px" }}></div>
-
-        {currentPage === "write" ? (
-          <>
-            <Button
-              ref={buttonRef}
-              variant="outlined"
-              className="border border-blue-950  p-1 bg-white focus:text-blue-900"
-              onClick={() => setCurrentPage("write")}
-            >
-              대본입력
-            </Button>
-            <Button
-              variant="outlined"
-              className="border border-blue-950 p-1  bg-gray-400 text-white"
-              onClick={() => setCurrentPage("select")}
-            >
-              대본선택
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button
-              ref={buttonRef}
-              variant="outlined"
-              className="border border-blue-950  p-1 bg-gray-400 text-white"
-              onClick={() => setCurrentPage("write")}
-            >
-              대본입력
-            </Button>
-            <Button
-              variant="outlined"
-              className="border border-blue-950 p-1  bg-white focus:text-blue-900"
-              onClick={() => setCurrentPage("select")}
-            >
-              대본선택
-            </Button>
-          </>
-        )}
-
-        {currentPage === "write" && <ScriptWrite />}
-        {currentPage === "select" && <ScriptSelect />}
-      </div>
-    </Page>
-=======
     <>
       <Page header={<Navbar />} footer={<Footer />}>
         <div style={{ paddingTop: "130px" }}></div>
@@ -128,7 +71,6 @@ const PracticePage = () => {
         </div>
       </Page>
     </>
->>>>>>> fe/feature/chatting
   );
 };
 
@@ -154,11 +96,7 @@ const ScriptWrite = () => {
           color="blue"
           placeholder="내용: 내용을 입력해 주세요. (최대 3000자)"
         />
-<<<<<<< HEAD
-        <Button className="bg-blue-950 text-white px-4 py-2 rounded z-10">
-=======
         <button className="bg-blue-950 text-white px-4 py-2 rounded ">
->>>>>>> fe/feature/chatting
           녹화 화면 가기 (나중에 라우터로 이동시키기)
         </button>
       </div>
