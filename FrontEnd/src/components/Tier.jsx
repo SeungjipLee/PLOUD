c9c7e70b058faa9d7f4f58ecfffc065c56b0d7b9
이승집 <edward626kr@gmail.com>
2024-02-07 16:06:00 +0900

    rebasetest

--- FrontEnd/src/components/Tier.jsx
+++ FrontEnd/src/components/Tier.jsx
@@ -56,3 +56,61 @@ const Tier = () => {
 };
 
 export default Tier;
+import React, { useEffect, useState } from 'react';
+import { getScoreList } from '../services/statistic';
+import { useSelector } from "react-redux";
+
+
+        
+
+const Tier = () => {
+    const { token } = useSelector((state) => state.userReducer)
+    const [ scoreList, setScoreList ] = useState([])
+    const [ isBronze, setIsBronze ] = useState(false)
+    const [ isSilver, setIsSilver ] = useState(false)
+    const [ isGold, setIsGold ] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await getScoreList(
+              token,
+              (res) => {
+                console.log(res.data.data.totalTime)
+                const tier = res.data.data.totalTime
+                if (tier < 60) {
+                    setIsBronze(true)
+                    setIsSilver(false)
+                    setIsGold(false)
+                } else if (tier<120) {
+                    setIsBronze(false)
+                    setIsSilver(true)
+                    setIsGold(false)
+                } else {
+                    setIsBronze(false)
+                    setIsSilver(false)
+                    setIsGold(true)
+                }
+
+            },
+              (err) => console.log('여기')
+            );       
+          } catch (error) {
+            console.error("쩌어기");
+          }
+        };
+        fetchData();
+      }, []);
+    
+    
+
+  return(
+    <>
+        {isBronze&& <img src='images/bronze.png'></img> }
+        {isSilver&& <img src='images/silver.png'></img> }
+        {isGold&& <img src='images/gold.png'></img> }
+    </>
+  )
+};
+
+export default Tier;
