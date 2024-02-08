98e25c2b533ed64bdb655063b4c800a720c57157
yeongdae <oooo_0@naver.com>
2024-02-08 10:43:41 +0900

    Fix: Video add muted

--- FrontEnd/src/pages/StudyRoomPage/index.jsx
+++ FrontEnd/src/pages/StudyRoomPage/index.jsx
@@ -391,14 +391,14 @@ const StudyRoomPage = () => {
         console.log("Session 연결중");
 
         let tmpPublisher = await OV.current.initPublisherAsync(undefined, {
-          audioSource: undefined, // The source of audio. If undefined default microphone
-          videoSource: undefined, // The source of video. If undefined default webcam
-          publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
-          publishVideo: true, // Whether you want to start publishing with your video enabled or not
-          resolution: "640x480", // The resolution of your video
-          frameRate: 30, // The frame rate of your video
-          insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
-          mirror: false, // Whether to mirror your local video or not
+          audioSource: undefined,
+          videoSource: undefined,
+          publishAudio: true,
+          publishVideo: true,
+          resolution: "640x480",
+          frameRate: 30,
+          insertMode: "APPEND",
+          mirror: false,
         });
 
         // --- 6) Publish your stream ---
