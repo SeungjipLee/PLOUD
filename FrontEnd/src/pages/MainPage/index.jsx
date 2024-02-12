1e66cb2cb6b7d05bb835743f8adbb9e024bd4396
김가영 <kkayoung0@gmail.com>
2024-02-12 21:21:12 +0900

    Design: 메인 페이지 스크롤 애니메이션 추가

--- FrontEnd/src/pages/MainPage/index.jsx
+++ FrontEnd/src/pages/MainPage/index.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Page from "../../components/Page";
 import Footer from "../../components/Footer";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import NoSkipResult from "../MyPage/NoSkipResult";
 
 
 const MainPage = () => {
@@ -23,37 +22,80 @@ const MainPage = () => {
   const handleClose = () => {
     setModal(false)
   }
+  
+  // scroll animation
+  useEffect(() => {
+    var scrollEvent = function() {
+      // 사용자 모니터 화면 높이 + 스크롤이 움직인 높이
+      var scroll = window.innerHeight + window.scrollY;
+      
+      // 애니메이션 효과를 넣을 DOM 객체 배열
+      var itemList = document.querySelectorAll(".animatable");
+       
+      Array.prototype.forEach.call(itemList, function(li){
+        // 객체 위치와 높이 비교 : 화면에 표출되는 높이인지 체크
+        if(li.offsetTop < scroll) {
+          // 객체 animatable 클래스 지우고, animated 클래스 추가
+          li.classList.remove("animatable");
+          li.classList.add("animated");
+        }
+      });
+    }
+
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener('scroll', scrollEvent);
+
+    // 컴포넌트가 언마운트될 때 리스너 제거
+    return () => {
+      window.removeEventListener('scroll', scrollEvent);
+    };
+  }, []);
+  // end scroll animation
+
   return (
-    <div className="Main">
+    <div>
       <Page header={<Navbar />} footer={<Footer />}>
-        <div className="Main1 mb-24">
-          <img src="images/2.png" className="w-full h-full object-cover"/>
+      {/* main1 */}
+      <div style={{height: "100vh", position:"relative"}} className="mainBlueB">
+      <img src="/images/main1transparent.png" alt="" style={{position:"absolute", top:"50%", left:"50%", transform:"translate(-50%, -50%)", zIndex:"1"}}/>
+        <div className="text-5xl font-extrabold mainOrangeF mainPageFont fadein" style={{position:"absolute", top:"35%", left:"50%", transform:"translate(-50%, -50%)", fontSize:'20vw'}}>
+          PLOUD
         </div>
-        <div className="Main2 mb-24">
-          <div className="video mx-16 ms-36 me-12">
-            <video src="/videos/mainvideo.mp4" autoPlay loop muted/>
-          </div>
+      </div>
+      {/* main2; video */}
+      <div style={{height: "100vh"}} className="Main2 bg-white">
+        <div className="video mx-16">
+          <video src="/videos/mainvideo.mp4" autoPlay loop muted></video>
+        </div>
+        <div>
           <div className='Main2Container'>
-            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">Carousel</div>
-            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5">오늘의 발표 수</div>
+            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5 animatable">
+              야이야이야
+            </div>
+            <div className="mainBlueB text-white card mx-16 me-36 ms-12 text-center pt-5 my-5 animatable">
+              간지나는 사진carousel
+            </div>
           </div>
         </div>
-        <div className="Main3">
-          <h2 className='text-5xl text-center pt-24 font-bold'>스피치 실력을 키워볼까요?</h2>
-          <div className='Main3_1 mt-32'>
-            <div className="subtitleImg me-12 mb-24" onClick={() => handleLink('/practice')}>
+      </div>
+      {/* main3; link */}
+      <div style={{ height: "100vh"}} className="bg-white">
+        <h2 className="text-5xl text-center font-bold pt-24">스피치 실력을 키워볼까요?</h2>
+        <div className='Main3_1 mt-32' >
+            <div className="subtitleImg me-12 mb-24 animatable" onClick={() => handleLink('/practice')}>
               <img src="images/solo.png"/>
-              <div className="text cursor-pointer">연습모드 바로가기</div>
+              <div className="text cursor-pointer font-bold ">연습모드 바로가기</div>
             </div>
-            <div className="subtitleImg mb-24" onClick={() => handleLink('/study')}>
+            <div className="subtitleImg mb-24 animatable" onClick={() => handleLink('/study')}>
               <img src="images/study.png"/>
-              <div className="text cursor-pointer">스터디룸 바로가기</div>
+              <div className="text cursor-pointer font-bold">스터디룸 바로가기</div>
             </div>
           </div>
-        </div>
-        <div className="Main1 mb-24">
-          <img src="images/evaluation2.PNG" className="w-full h-full object-cover"/>
-        </div>
+      </div>
+      {/* main4; about us */}
+      <div style={{height: "100vh", position:"relative"}} className="bg-white animatable">
+        <img src="images/AboutUs.png" alt="about us" style={{position:"absolute", top:"50%", left:"50%", transform:"translate(-50%, -50%)"}}/>
+      </div>
       </Page>
     </div>
   );
