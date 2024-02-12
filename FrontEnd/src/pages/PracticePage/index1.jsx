7d41e62f04f61c882f623e4c5fd31ecb5a5108fb
yeongdae <oooo_0@naver.com>
2024-02-13 00:57:11 +0900

    Fix

--- FrontEnd/src/pages/PracticePage/index1.jsx
+++ FrontEnd/src/pages/PracticePage/index1.jsx
@@ -4,9 +4,6 @@ import Footer from "../../components/Footer";
 
 const PracticePage1 = () => {
 
-
-
-
     return (
       <div className="bg-white w-full min-h-screen">
           <Page header={<Navbar />} footer={<Footer />}>
