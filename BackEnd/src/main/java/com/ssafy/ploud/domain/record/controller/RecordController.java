93cf4458368daf035cf47f4354da13e7f4cc1d85
yeongdae <oooo_0@naver.com>
2024-02-13 16:37:39 +0900

    Fix: Speech Test Error

--- BackEnd/src/main/java/com/ssafy/ploud/domain/record/controller/RecordController.java
+++ BackEnd/src/main/java/com/ssafy/ploud/domain/record/controller/RecordController.java
@@ -34,7 +34,7 @@ public class RecordController {
     @Operation(summary = "스피치 결과 조회", description = "결과 페이지에 나타낼 정보들을 조회한다.")
     @GetMapping("/{speechId}")
     public ApiResponse<?> detailSpeech(@PathVariable("speechId") int speechId){
-        log.debug("스피치 결과 조회 스피치 아이디 : " + speechId);
+        log.debug("스피치 결과 조회, 스피치 아이디 : " + speechId);
         return ApiResponse.ok("성공", recordService.getSpeechDetail(speechId));
     }
 
