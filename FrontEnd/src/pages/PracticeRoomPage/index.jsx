c0795bf3e6131b229f34dafdd6c2c101dc2af224
yeongdae <oooo_0@naver.com>
2024-02-16 01:38:41 +0900

    Fix: 연습 페이지 비디오 재렌더링 오류 수정

--- FrontEnd/src/pages/PracticeRoomPage/index.jsx
+++ FrontEnd/src/pages/PracticeRoomPage/index.jsx
@@ -115,28 +115,30 @@ const PracticeRoomPage = () => {
     // 평가 요청을 받았을 떄 속도가 빠르다, 발음 점수가 낮다.
     // 실시간 데시벨 측정으로 목소리 크기
 
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-
-          const vRecorder = new MediaRecorder(stream);
-          setStream(stream);
-          setVideoRecorder(vRecorder);
-
-          vRecorder.ondataavailable = (e) => {
-            if (e.data.size > 0) {
-              // 주석해제하기
-              videoUpload(e.data); // e.data : videoChunk
-            }
-          };
-        })
-        .catch((error) => {
-          // console.log(error);
-        });
+    if(video){
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({ video: true, audio: true })
+          .then((stream) => {
+            videoRef.current.srcObject = stream;
+  
+            const vRecorder = new MediaRecorder(stream);
+            setStream(stream);
+            setVideoRecorder(vRecorder);
+  
+            vRecorder.ondataavailable = (e) => {
+              if (e.data.size > 0) {
+                // 주석해제하기
+                videoUpload(e.data); // e.data : videoChunk
+              }
+            };
+          })
+          .catch((error) => {
+            // console.log(error);
+          });
+      }
     }
-
+    
     return () => {
       if (stream) {
         stream.getTracks().forEach((track) => track.stop());
