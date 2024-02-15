e15f98e06131cf721124bfd8422e60577b74d154
pmhmist <pmhmist@naver.com>
2024-02-15 09:43:31 +0900

    FIX: 스터디룸 div 수정

--- FrontEnd/src/pages/StudyRoomPage/component/Loading.jsx
+++ FrontEnd/src/pages/StudyRoomPage/component/Loading.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const LoadingScreen = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div
+      className="flex justify-center items-center"
+      style={{ width: "800px", height: "400px" }}
+    >
       <div
         className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500"
-        style={{ borderTopColor: 'transparent' }}
+        style={{ borderTopColor: "transparent" }}
       ></div>
     </div>
   );
