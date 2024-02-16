ed95930ecc672976fb8f64686b01ff86017d6569
김가영 <kkayoung0@gmail.com>
2024-02-16 11:40:14 +0900

    Update exec/dummy_data.sql

--- exec/dummy_data.sql
+++ exec/dummy_data.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `boards`
 --
 
+CREATE DATABASE ploud_db;
+
 DROP TABLE IF EXISTS `boards`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
